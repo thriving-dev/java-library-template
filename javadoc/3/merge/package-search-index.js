2a94e2310cab66d4cbc679bf0b4d65c5ada1ca72
Hartmut <info@hartmut.co.uk>
2023-09-11 19:03:02 +0200

    Delete javadoc/3 directory

--- javadoc/3/merge/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dev.thriving.oss.java.library.template"}];updateSearchResults();
--- javadoc/3/merge/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"dev.thriving.oss.java.library.template","l":"JavaLibraryTemplate"}];updateSearchResults();
